bfccb53bb0ad3eb8299cc5d8742bfa10a5819b6f
sandeep-v1404 <valpasanisandeep@gmail.com>
2021-04-08 10:33:01 +0530

    Container widget

--- lib/main.dart
+++ lib/main.dart
@@ -15,8 +15,28 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Awesome App 1"),
       ),
-      body: Container(
-        child: Text("Hi Flutter"),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          width: 100,
+          height: 100,
+          clipBehavior: Clip.antiAlias,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+              color: Colors.red,
+              borderRadius: BorderRadius.circular(10),
+              gradient: LinearGradient(colors: [Colors.pink, Colors.yellow]),
+              boxShadow: [BoxShadow(color: Colors.grey, blurRadius: 10)]),
+          child: Text(
+            "I am a Box",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
       ),
     );
   }
